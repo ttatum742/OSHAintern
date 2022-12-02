7fcc4c7304c9943edca2ec2bb16dd066a634af13
ThomasTatum <thomastatum98@gmail.com>
2022-12-01 19:49:42 -0600

    geodat in

---
+++ dev/sql/geodat.sql
@@ -0,0 +1,8 @@
+SELECT
+  SIR2015to22.EventDate
+ ,SIR2015to22.Latitude
+ ,SIR2015to22.Longitude
+ ,SIR2015to22.[Primary NAICS]
+ ,SIR2015to22.EventTitle
+ ,SIR2015to22.Employer
+FROM dbo.SIR2015to22
